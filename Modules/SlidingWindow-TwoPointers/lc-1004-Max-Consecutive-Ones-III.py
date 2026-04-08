0769287f31fc8b3c7ec2cad9d779fd0661f301ea
Pritam <pritamsahaofficial95@gmail.com>
2026-04-08 15:23:48 +0530

    lc 1004 striver

---
+++ Modules/SlidingWindow-TwoPointers/lc-1004-Max-Consecutive-Ones-III.py
@@ -0,0 +1,45 @@
+'''
+    Problem: https://leetcode.com/problems/max-consecutive-ones-iii/
+'''
+
+class Solution:
+    def longestOnes(self, nums: list[int], k: int) -> int:
+
+        ##brute
+        maxLen = 0
+        n = len(nums)
+
+        for i in range(n):
+            zeroCnt = 0
+            for j in range(i, n):
+                if nums[j] == 0:
+                    zeroCnt += 1
+                if zeroCnt <= k:
+                    maxLen = max(maxLen, j - i + 1)
+                else:
+                    break
+        return maxLen
+
+
+    def longestOnesOptimal(self, nums: list[int], k: int) -> int:
+        '''
+            Two pointer + sliding window
+        '''
+        n = len(nums)
+        l, r = 0, 0
+        maxLen = 0
+        zeroCount = 0
+
+        while r < n:
+            if nums[r] == 0:
+                zeroCount += 1
+
+            while zeroCount > k:
+                if nums[l] == 0:
+                    zeroCount -= 1
+                l += 1
+            if zeroCount <= k:
+                maxLen = max(maxLen, r-l+1)
+            r += 1
+        return maxLen
+        
