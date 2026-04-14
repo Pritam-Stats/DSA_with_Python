66447b9ee06ea1f9cdb33d12fde9de09ab043e87
Pritam <pritamsahaofficial95@gmail.com>
2026-04-14 14:16:30 +0530

    bisect module

--- Modules/Binary-Search/lc-704-binary-search.py
+++ Modules/Binary-Search/lc-704-binary-search.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
         n = len(nums)
@@ -14,3 +15,12 @@ class Solution:
                 l = m+1
 
         return -1
+    
+    def search2(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+
+        return bisect_left(nums, target)
+    
+s = Solution()
+print(s.search([1,2,3,4,4,4,5], 4))
+print(s.search2([1,2,3,4,4,4,5], 4))
