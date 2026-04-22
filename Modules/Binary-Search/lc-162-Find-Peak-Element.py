1521fccc53cae454695e9bf559ab130b167e64ab
Pritam <pritamsahaofficial95@gmail.com>
2026-04-22 14:54:21 +0530

    updated

--- Modules/Binary-Search/lc-162-Find-Peak-Element.py
+++ Modules/Binary-Search/lc-162-Find-Peak-Element.py
@@ -5,6 +5,42 @@
 Key Intuition: if we are declining in a mountain means we already had a peak in our left,
 if uphill, we will get a peak.
 '''
+
+#--------------------------------------------------------------------------
+
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        if nums[0] > nums[1]:
+            return 0
+        if nums[-1] > nums[-2]:
+            return n-1
+        
+        l, r = 1, n-2
+        while l <= r:
+            mid = l + ((r - l)//2)
+
+            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid -1]:
+                return mid
+            elif nums[mid] < nums[mid+1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------
+
+
+
 class Solution:
     ## NeetCode
     def findPeakElement(self, nums: list[int]) -> int:
