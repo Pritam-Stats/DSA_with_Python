04470b0e62e50da8059fb34fecb8533678775542
Pritam <pritamsahaofficial95@gmail.com>
2026-05-06 16:28:17 +0530

    power of 2

---
+++ Modules/Bit-Manipulation/231.power-of-two.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=231 lang=python3
+#
+# [231] Power of Two
+#
+
+# @lc code=start
+
+
+# ================================
+# Author: Pritam
+# ================================
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n>0 and n&n-1 == 0
+# @lc code=end
+
